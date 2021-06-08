84f643d3887db6ec6242d3068447690fd3cbf7aa
Skyost <Skyost@users.noreply.github.com>
2021-06-08 22:25:41 +0000

    deploy: e5c70ef91fe09fcda0f32f608faf73d50226e657

--- _nuxt/static/1621088528/article/hello-world/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621088528",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Fhello-world",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0abd60b1"}}}}(null));
